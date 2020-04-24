0e816b9e5d9105c3840d3e0aecb9f9e11b839564
DevNatan <itroken99@gmail.com>
2020-04-24 18:35:23 -0300

    Remove the deprecated game module.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { HappyShop } from "./hs";
+import {HappyShop} from "./hs";
+
+export interface HSEntity {
+
+    readonly createdAt: Date
+
+    readonly updatedAt: Date
+
+}
 
 export * from "./types/payment"
 export * from "./types/user"
-export * from "./types/game"
 export * from "./types/theme"
 export * from "./types/store"
 export * from "./config";
